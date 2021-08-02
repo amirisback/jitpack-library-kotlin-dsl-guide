0723b016068e5af56c15ed7f8f892be84efbddf0
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-08-02 10:33:55 +0700

    update: JAVA_VERSION

---
+++ jitlib/src/main/java/com/frogobox/test/JitlibApps.kt
@@ -0,0 +1,15 @@
+package com.frogobox.test
+
+/*
+ * Created by faisalamir on 02/08/21
+ * JitpackLibTest
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+ 
