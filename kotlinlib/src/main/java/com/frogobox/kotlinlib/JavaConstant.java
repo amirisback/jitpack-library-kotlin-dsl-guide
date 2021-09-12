f3904abc1ccaef21d0b0ad0eeafe341920c16576
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-09-12 14:08:13 +0700

    add: java/kotlin library

---
+++ kotlinlib/src/main/java/com/frogobox/kotlinlib/JavaConstant.java
@@ -0,0 +1,23 @@
+package com.frogobox.kotlinlib;
+
+/*
+ * Created by faisalamir on 12/09/21
+ * JitpackLibTest
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.
+ * All rights reserved
+ *
+ */
+
+public class JavaConstant {
+
+    public static class Const {
+        String TAG = "JavaConstant";
+        String TYPE_CLASS = "Java Constant";
+    }
+
+}
---
+++ kotlinlib/src/main/java/com/frogobox/kotlinlib/KotlinConstant.kt
@@ -0,0 +1,8 @@
+package com.frogobox.kotlinlib
+
+object KotlinConstant {
+
+    const val TAG = "Kotlin Contant"
+    const val TYPE_CLASS = "Kotlin"
+
+}
